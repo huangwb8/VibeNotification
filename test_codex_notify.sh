6205198959df68bc101fdc6a7dedf300ab4ccbf5
Weibin Huang <654751191@qq.com>
2025-12-22 00:25:31 +0800

    Delete test_codex_notify.sh

--- test_codex_notify.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# 测试 Codex notify 是否被调用
-
-echo "$(date): Codex notify 被调用" >> /tmp/codex_notify_test.log
-echo "参数: $@" >> /tmp/codex_notify_test.log
-echo "---" >> /tmp/codex_notify_test.log
-
-# 调用实际的通知系统
-python3 -m vibe_notification "$@"
